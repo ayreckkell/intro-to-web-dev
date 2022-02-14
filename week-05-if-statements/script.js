dc8472efd40afd976d46874046ed481327a1cc08
ayreckkell <97994726+ayreckkell@users.noreply.github.com>
2022-02-14 15:39:42 -0500

    Finish Making quiz and making sure all elements work in website

--- week-05-if-statements/script.js
+++ week-05-if-statements/script.js
@@ -6,14 +6,14 @@ function submitAnswerOne() {
   // Get the text from answer one's input element
   const answerOne = answerOneInput.value;
 
-  if(answerOne == 'cat') {
+  if(answerOne == 'force') {
     alert("That's right!");
     hide('question-one');
     show('question-two');
-  } else if(answerOne == 'dog') {
-    alert('Close, but my favorite animal runs away from dogs.');
+  } else if(answerOne == 'Schwartz') {
+    alert('That is from Spaceball. Different movie but you are on the right track');
   } else {
-    alert('Wrong! Hint: My favorite animal meows.')
+    alert('Wrong! Hint: It starts with a f')
   }
 }
 
@@ -25,15 +25,127 @@ function submitAnswerTwo() {
   // Get the number from answer two's input element
   const answerTwo = answerTwoInput.value;
 
-  if(answerTwo == 42) {
+  if(answerTwo == 'Darth Sidious'  ) {
     alert("That's right!");
     hide('question-two');
-    show('done');
-  } else if(answerTwo < 42) {
-    alert('The answer is higher.');
+    show('question-three');
+  } else if(answerTwo == 'Anakin Skywalker') {
+    alert('Not quite, but the guy did influnce him to become a villian');
   } else {
-    alert('The answer is lower.')
+    alert('His a dark lord of the sith')
   }
+}
+
+function submitAnswerThree(){
+  const answerThreeInput = document.getElementById('answer-three-input');
+    const answerThree = answerThreeInput.value;
+      if(answerThree == 'Anakin Skywalker') {
+          alert("That's right!");
+          hide( 'question-three');
+          show( 'question-four');
+      } else if ( answerThree == 'Luke Skywalker'){
+          alert('Close but that the son you want the father');
+      } else {
+        alert ('He is a Skywalker')
+      }
+}
+
+function submitAnswerFour(){
+  const answerFourInput = document.getElementById('answer-four-input');
+    const answerFour = answerFourInput.value;
+      if(answerFour == 'Slave 1') {
+          alert("That's right!");
+          hide( 'question-four');
+          show( 'question-five');
+      } else if ( answerFour == 'Slave'){
+          alert('Almost you forgot the second part');
+      } else {
+        alert ('Sorry no hint here just google it')
+      }
+}
+
+function submitAnswerFive(){
+  const answerFiveInput = document.getElementById('answer-five-input');
+    const answerFive = answerFiveInput.value;
+      if(answerFive == 'Han') {
+          alert("That's right!");
+          hide( 'question-five');
+          show( 'question-six');
+      } else if ( answerFive == 'Luke'){
+          alert('HEY THAT DID NOT HAPPEN. TRY AGAIN');
+      } else {
+        alert ('He is one of the best piolts in the galaxy')
+      }
+}
+
+function submitAnswerSix(){
+  const answerSixInput = document.getElementById('answer-six-input');
+    const answerSix = answerSixInput.value;
+      if(answerSix == 'Anakin Skywalker') {
+          alert("That's right!");
+          hide( 'question-six');
+          show( 'question-seven');
+      } else if ( answerSix == 'Luke Skywalker'){
+          alert('Have you seen the movies? Luke is the one fighting Darth Vader. Try the other Skywalker');
+      } else {
+        alert ('He is a Skywalker')
+      }
+}
+
+function submitAnswerSeven(){
+  const answerSevenInput = document.getElementById('answer-seven-input');
+    const answerSeven = answerSevenInput.value;
+      if(answerSeven == 'Kylo Ren') {
+          alert("That's right!");
+          hide( 'question-seven');
+          show( 'question-eight');
+      } else if ( answerSeven == 'Darth Vader'){
+          alert('Not quite, he is Darth Vaders Grandson');
+      } else {
+        alert ('This character is know for his corss-guard lightsaber')
+      }
+}
+
+function submitAnswerEight(){
+  const answerEightInput = document.getElementById('answer-eight-input');
+    const answerEight = answerEightInput.value;
+      if(answerEight == 'Darth Bane') {
+          alert("That's right!");
+          hide( 'question-eight');
+          show( 'question-nine');
+      } else if ( answerEight == 'Darth Sidious'){
+          alert('Not Sidious but he is a Sith Lord');
+      } else {
+        alert ('He is a Sith Lord')
+      }
+}
+
+function submitAnswerNine(){
+  const answerNineInput = document.getElementById('answer-nine-input');
+    const answerNine = answerNineInput.value;
+      if(answerNine == 'Boba Fett') {
+          alert("That's right!");
+          hide( 'question-nine');
+          show( 'question-ten');
+      } else if ( answerNine == 'Jango Fett'){
+          alert('My guy he is dead. What is the sone name.');
+      } else {
+        alert ('The only character that is not even a main character in his own show')
+      }
+}
+
+function submitAnswerTen(){
+  const answerTenInput = document.getElementById('answer-ten-input');
+    const answerTen = answerTenInput.value;
+      if(answerTen == 'Hutt') {
+          alert("That's right!");
+          hide( 'question-ten');
+          show( 'done');
+      } else if ( answerTen == 'Big Worm'){
+          alert('Your not wrong but what is the species name');
+      } else {
+        alert ('Jabba lives on the dessert Planet')
+      }
 }
 
 // Helper function that takes an ID and shows the element with that ID.
