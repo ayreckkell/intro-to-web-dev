997edf2f6dacf4e916313a7d91d2974eed7c891f
ayreckkell <97994726+ayreckkell@users.noreply.github.com>
2022-02-28 13:28:29 -0500

    added first section but second section is not working

--- week-06-for-loops/script.js
+++ week-06-for-loops/script.js
@@ -1,12 +1,23 @@
 // Replace this speeh with your own.
 // Notice the `` backticks, which let you split your string into multiple lines.
-const speech = `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-    eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-    veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-    commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit
-    esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-    cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-    est laborum.`;
+const speech = `
+  On to battle
+  Here we are the hunters
+  Red as fire
+  Crimson Bow and Arrow
+  Waiting for the storm won't help you
+  Just the will to fight can change you
+  Clean your weapons blow the dust off
+  Fear and prayers will never save us
+
+  We can't hide our pride behind fear
+  We will not let them be this near
+  In the end our strength unites us
+  Just the will to fight will save us
+  No, we will never ever fail
+  We stand up and we fight for the moment of the truth
+  Stand up and fight for their demise
+  We won't surrender`;
 
 // Remove punctuation from the speech. You might have to modify this if your
 // speech contains other punctuation.
@@ -33,12 +44,49 @@ function displayLongWords() {
   }
 }
 
+
+function displayShortWords(){
+  const shortWordsElement = document.getElementById('short-words');
+
+  for(let i = 0; i < wordsArray.length; i++) {
+      const word = wordsArray[i];
+
+      if(word.length < 4) {
+        const wordElement = document.createElement('li');
+        wordElement.innerText = word;
+        shortWordsElement.appendChild(wordElement);
+      }
+  }
+}
+
+function displayNameWords(){
+  const nameWordsElement = document.getElementById('name-words');
+
+  for(let i = 0; i < wordsArray.length; i++) {
+      const word = wordsArray[i];
+
+      if(word[i].startsWith ('A')) {
+        const wordElement = document.createElement('li');
+        wordElement.innerText = word;
+        nameWordsElement.appendChild(wordElement);
+      }
+  }
+}
+
+
+
+
+
 // TODO: Define your own functions here!
 
 function displaySpeechStats() {
   document.getElementById('speech').innerText = speech;
 
   displayLongWords();
+
+  displayShortWords();
+
+  displayNameWords();
 
   // TODO: Call your functions here!
 }
