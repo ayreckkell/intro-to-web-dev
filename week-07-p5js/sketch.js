f70b8be4657257edcafb01f1236ed060fa70abee
ayreckkell <97994726+ayreckkell@users.noreply.github.com>
2022-03-14 15:20:30 -0400

    falling snow working

--- week-07-p5js/sketch.js
+++ week-07-p5js/sketch.js
@@ -1,7 +1,30 @@
+let circleY =[];
+let ySpeed =[];
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(500, 500);
+  for(let i = 0; i < 100; i++) {
+    circleY[i] = random(height);
+    ySpeed[i]= random(.5,2);
+
+
+  }
 }
 
 function draw() {
-  background(220);
+  background(75);
+
+  for (let i = 0; i < circleY.length; i++) {
+    let circleX = width * i /circleY.length;
+    circle(circleX, circleY[i], 10);
+
+    circleY[i] += ySpeed[i];
+
+    if (circleY[i] > height){
+      circleY[i] = 0;
+    }
+
+
+  }
+
 }
--- week-zz-scratch/script.js
+++ week-zz-scratch/script.js
@@ -1,3 +1,5 @@
-function load() {
-  console.log('Hello world!');
+let total = 0;
+for (let number = 1; number <= 10; number++) {
+  total += number;
 }
+console.log(total);
