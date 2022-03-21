381c91932a5ecce6511109ef95d451036611cfea
ayreckkell <97994726+ayreckkell@users.noreply.github.com>
2022-03-21 17:45:29 -0400

    Finish my list

--- week-08-objects/script.js
+++ week-08-objects/script.js
@@ -1,27 +1,68 @@
 // Array of objects representing a todo list.
 // Modify this array to contain your own list.
-const taskArray = [
-  {label: 'Water plants', time: 3},
-  {label: 'Homework', time: 2},
-  {label: 'Laundry', time: 1},
+const bloodborneArray = [
+  {bossname: 'Orphan Of Kos',
+   Location: 'Lighthouse Hut' ,
+   Difficulty: 8.5 + "/" + 10},
+
+  {bossname: 'Ludwig',
+   Location: 'Hunters Nightmare',
+   Difficulty: 7 + "/" + 10},
+
+  {bossname: 'Gehrman, The First Hunter',
+   Location: 'Hunters Dream',
+   Difficulty: 9 + "/" + 10},
+
+  {bossname: 'Martyr Logarius',
+   Location: 'Forsaken Cainhurst Castle' ,
+   Difficulty: 8 + "/" + 10},
+
+  {bossname: 'Cleric Beast',
+   Location: 'Central Yharnam The Great Bridge' ,
+   Difficulty: 5 + "/" + 10},
+
+  {bossname: 'Father Gascoigne',
+   Location: 'Cathedral Ward Great Cathedral' ,
+   Difficulty: 6 + "/" + 10},
+
+  {bossname: 'Vicar Amelia',
+   Location: 'Central Yharnam Oedons Tombe' ,
+   Difficulty: 4.5 + "/" + 10},
+
+  {bossname: 'Blood-Starved Beast',
+   Location: 'Hemick Charnel Lane Witchs Abode' ,
+   Difficulty: 6.5 + "/" + 10},
+
+  {bossname: 'The Witch of Hemwick',
+   Location: 'Old Yharnam Church of the Good Chalice' ,
+   Difficulty: .5 + "/" + 10},
+
+  {bossname: 'Darkbeast Paarl',
+   Location: 'Graveyard of the Darkbeast' ,
+   Difficulty: 5 + "/" + 10},
+
+
+
 ];
 
 // Loads the content into the page.
 function loadContent() {
-  // This line of code sorts the array alphabetically by the task labels.
+  // This line of code sorts the array alphabetically by the task bossnames.
   // Modify this to sort your data by a different field, or just delete it.
-  taskArray.sort((a, b) => compare(a.label, b.label));
+  bloodborneArray.sort((a, b) => compare(a.Difficulty, b.Difficulty));
 
   loadTable();
   loadShortestTask();
+  loadAverage();
 }
 
 // Adds a task to the array and reloads the page content.
 function addNewTask() {
-  const newTaskLabel = document.getElementById('label-input').value;
-  const newTaskTime = document.getElementById('time-input').value;
-  const newTask = {label: newTaskLabel, time: newTaskTime };
-  taskArray.push(newTask);
+  const newTaskbossname = document.getElementById('bossname-input').value;
+  const newTaskDifficulty = document.getElementById('Difficulty-input').value + '/10';
+  const newTaskLocation = document.getElementById('Location-input').value;
+  const newTask = {bossname: newTaskbossname, Difficulty: newTaskDifficulty, Location: newTaskLocation };
+  bloodborneArray.push(newTask);
 
   loadContent();
 }
@@ -32,18 +73,18 @@ function loadTable() {
 
   // Create a header row.
   const headerRowElement = document.createElement('tr');
-  headerRowElement.appendChild(createElement('th', 'Index'));
-  headerRowElement.appendChild(createElement('th', 'Label'));
-  headerRowElement.appendChild(createElement('th', 'Time'));
+  headerRowElement.appendChild(createElement('th', 'Boss Name'));
+  headerRowElement.appendChild(createElement('th', 'Location'));
+  headerRowElement.appendChild(createElement('th', 'Difficulty'));
   tableElement.appendChild(headerRowElement);
 
   // Iterate over the array and create a table row for each object.
-  for (let i = 0; i < taskArray.length; i++) {
-    const task = taskArray[i];
+  for (let i = 0; i < bloodborneArray.length; i++) {
+    const task = bloodborneArray[i];
     const rowElement = document.createElement('tr');
-    rowElement.appendChild(createElement('td', i));
-    rowElement.appendChild(createElement('td', task.label));
-    rowElement.appendChild(createElement('td', task.time));
+    rowElement.appendChild(createElement('td', task.bossname));
+    rowElement.appendChild(createElement('td', task.Location));
+    rowElement.appendChild(createElement('td', task.Difficulty));
     tableElement.appendChild(rowElement);
   }
 
@@ -55,18 +96,41 @@ function loadTable() {
 // Displays the name of the shortest task.
 function loadShortestTask(){
   // Assume the first task is shortest
-  let shortestTask = taskArray[0];
+  let shortestTask = bloodborneArray[0];
 
   // Starting with the second task, look for a shorter task
-  for (let i = 1; i < taskArray.length; i++) {
-    const task = taskArray[i];
+  for (let i = 1; i < bloodborneArray.length; i++) {
+    const task = bloodborneArray[i];
     // If this task is shorter than the previous shortest, it's now the shortest
-    if(task.time < shortestTask.time) {
+    if(task.Difficulty < shortestTask.Difficulty) {
       shortestTask = task;
     }
   }
-  document.getElementById('shortest-task').innerText = shortestTask.label;
+  document.getElementById('shortest-task').innerText = shortestTask.bossname;
 }
+
+
+
+
+// Displays the name of the Average task.
+function loadAverage(){
+  let total = 0;
+ for(let i = 0; i < bloodborneArray.length; i++){
+    console.log (total)
+   const boss = bloodborneArray[i];
+    total += Number(boss.Difficulty.split('/')[0]);
+
+ }
+  let average = total / bloodborneArray.length;
+  console.log (average);
+  document.getElementById('average-task').innerText = average
+  }
+
+
+
+
+
+
 
 // Helper function that creates an element that contains text content.
 function createElement(tag, text) {
